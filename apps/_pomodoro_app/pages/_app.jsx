d4977f55c607bb56192187376fa564149af134f8
tramakarov <e2042912@gmail.com>
2020-06-19 00:34:06 +0500

    Update _app.jsx

--- apps/_pomodoro_app/pages/_app.jsx
+++ apps/_pomodoro_app/pages/_app.jsx
@@ -7,7 +7,7 @@ export default function App ({ Component, pageProps }) {
         <Container>
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"/>
-                <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
                 <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"/>
             </Head>
             <Component {...pageProps} />
