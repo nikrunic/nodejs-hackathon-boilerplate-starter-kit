058b747dfcc58cc052aa8ba44c1aa6b1842446e3
tramakarov <e2042912@gmail.com>
2020-06-29 11:34:37 +0500

    Update Layout.jsx

--- apps/_pomodoro_app/components/Layout.jsx
+++ apps/_pomodoro_app/components/Layout.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Header from './Header'
 
-export default function Layout({ children, home }) {
-    return (
-        <div className="container">
-            <Header/>
-            <div className="page-content">
-                <main>{children}</main>
+export default function Layout(props) {
+    if (props.justifyCenter === false) {
+        return (
+            <div className="container">
+                <Head>
+                    <link rel="icon" type="image/x-icon" href="/brand/favicon.ico" />
+                </Head>
+                <Header/>
+                <main>{props.children}</main>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className="container">
+                <Header/>
+                <div className="page-content">
+                    <main>{props.children}</main>
+                </div>
+            </div>
+        )
+    }
 }
