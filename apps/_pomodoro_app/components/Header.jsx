d38f1219aecd0e6d2ae975e56c67bf11d7f7fdf1
tramakarov <e2042912@gmail.com>
2020-06-29 11:34:45 +0500

    Add link in header

--- apps/_pomodoro_app/components/Header.jsx
+++ apps/_pomodoro_app/components/Header.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import PomodoroIcon from '../public/icons/PomodoroIcon'
 import { Divider } from 'antd'
 import './Header.less'
+import Link from 'next/link'
+
 
 
 export default function Header() {
     return(
         <React.Fragment>
             <div className="header_icon">
-                <PomodoroIcon/>
+                <Link href="/">
+                    <PomodoroIcon/>
+                </Link>
             </div>
             <Divider className="header_divider"/>
         </React.Fragment>
